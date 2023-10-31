630f68d9407e5406de1babf64e4c19ed25add3de
devseop <cocc263@gmail.com>
2023-10-31 22:50:11 +0900

    :art: Gatsby 관련 파일 수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -71,7 +71,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   // Import Post Template Component
   const PostTemplateComponent = path.resolve(
     __dirname,
-    'src/templates/post_template.tsx',
+    'src/templates/PostTemplate.tsx',
   );
 
   // Page Generate Function
